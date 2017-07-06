e23e3acd406a5c9709d633953e5ede025c9afc2a
amit4alljava <amit4alljava@gmail.com>
2017-07-06 13:19:21 +0530

    Codes

---
+++ Code10/dave2/dave/GameConstant.java
@@ -0,0 +1,13 @@
+package com.bmpl.dave;
+
+public interface GameConstant {
+	int GAME_WIDTH = 900;
+	int GAME_HEIGHT = 600;
+	int GAME_SPEED  = 50;
+	String TITLE = "DAVE GAME - 2017";
+	int FLOOR = 481;
+	String PLAYER_IMG = "player.gif";
+	String BG_IMG = "bg.png";
+	int MAX_ENEMY = 2;
+	int GRAVITY = 1;
+}
