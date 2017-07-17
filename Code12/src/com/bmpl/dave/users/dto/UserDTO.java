d77a4cb203205941bf03407d8ee4036acb2fb6c7
amit4alljava <amit4alljava@gmail.com>
2017-07-17 13:04:50 +0530

    Codes

---
+++ Code12/src/com/bmpl/dave/users/dto/UserDTO.java
@@ -0,0 +1,43 @@
+package com.bmpl.dave.users.dto;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+	private String userid;
+	private String password;
+	private String gender;
+	private String city;
+	private String address;
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
